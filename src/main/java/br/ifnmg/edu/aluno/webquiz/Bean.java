78ea5117efe144894dbeb8dc190dd7101571f2d8
Lucas <IFNMG@LABCOMP1-COM-07>
2023-05-10 16:25:36 -0300

    Classe de marcação

---
+++ src/main/java/br/ifnmg/edu/aluno/webquiz/Bean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package br.ifnmg.edu.aluno.webquiz;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Lucas Freitas &lt;lpf1 at ifnmg.edu.br&gt;
+ */
+@Named(value = "bean")
+@Dependent
+public class Bean {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean() {
+    }
+    
+}
