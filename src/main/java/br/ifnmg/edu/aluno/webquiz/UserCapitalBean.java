14db20ca7ed23b3c76db1ac577e846da50b9ed80
hudso <hudso@192.168.82.188>
2023-05-14 17:49:02 -0300

    Tentando Resolver o problema da Resposta

--- src/main/java/br/ifnmg/edu/aluno/webquiz/UserCapitalBean.java
+++ src/main/java/br/ifnmg/edu/aluno/webquiz/UserCapitalBean.java
@@ -15,13 +15,16 @@ public class UserCapitalBean implements Serializable {
     private String paisSelecionado;
     private String capitalSelecionada;
     private String resposta;
+    private String respostaCorreta;
+    private String respostaIncorreta;
 
-    public UserCapitalBean(String paisSelecionado, String capitalSelecionada, String resposta) {
+    public UserCapitalBean() {
         this.paisSelecionado = paisSelecionado;
         this.capitalSelecionada = capitalSelecionada;
         this.resposta = resposta;
     }
 
+    
     
     
     //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
@@ -49,17 +52,58 @@ public class UserCapitalBean implements Serializable {
         this.resposta = resposta;
     }
 
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public void setRespostaCorreta(String respostaCorreta) {
+        this.respostaCorreta = respostaCorreta;
+    }
+
+    public String getRespostaIncorreta() {
+        return respostaIncorreta;
+    }
+
+    public void setRespostaIncorreta(String respostaIncorreta) {
+        this.respostaIncorreta = respostaIncorreta;
+    }
+    
+
 //</editor-fold>
     
     public String verificarResposta(){
         if("brasil".equals(paisSelecionado) && "brasilia".equals(capitalSelecionada)){
            resposta = "Resposta Correta!";
+        }else if("eua".equals(paisSelecionado) && "washington".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("franca".equals(paisSelecionado) && "paris".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("japao".equals(paisSelecionado) && "toquio".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("australia".equals(paisSelecionado) && "sidney".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("argentina".equals(paisSelecionado) && "buenos aires".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("belgica".equals(paisSelecionado) && "bruxelas".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("canada".equals(paisSelecionado) && "otava".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else if("china".equals(paisSelecionado) && "pequim".equals(capitalSelecionada)){
+            resposta = "Resposta Correta!";
+        }else{
+            resposta = "Resposta Incorreta!";
         }
-        
         
         
         return "resultado";
     }
     
+        public String imprimeResposta() {
+        if ("Resposta Correta!".equals(resposta)) {
+            return "Resposta Correta!";
+        } else {
+            return "Resposta Incorreta!";
+        }
+    }
     
 }
